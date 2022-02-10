aa21fa3eede0ad344631cf3d2b18fca8323d2f00
Gerard Compion <gcompion93@gmail.com>
2022-02-10 14:17:24 -0500

    change getAllProperties to take filters via search interface

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -90,7 +90,7 @@ const getAllReservations = (guest_id, limit = 10) => {
     `, values)
     .then(res => res.rows.length > 0 ? res.rows : null)
     .catch(err => console.log(err.message));
-}
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
@@ -102,14 +102,35 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`
-    SELECT *
-    FROM properties
-    LIMIT $1
-    `, [limit])
-    .then(res => res.rows.length > 0 ? res.rows : null)
-    .catch((err) => console.log(err.message));
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+   SELECT properties.*, avg(property_reviews.rating) as average_rating
+   FROM properties
+   JOIN property_reviews ON properties.id = property_id
+   `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+   GROUP BY properties.id
+   ORDER BY cost_per_night
+   LIMIT $${queryParams.length};
+   `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
+    .then((res) => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
